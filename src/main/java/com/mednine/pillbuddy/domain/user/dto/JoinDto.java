28cda670b9afe5047b178d7c1ae5af802e87317c
hongjeZZ <anhongje98@naver.com>
2024-09-27 17:24:21 +0900

    test: UserService 테스트 코드 작성

--- src/main/java/com/mednine/pillbuddy/domain/user/dto/JoinDto.java
+++ src/main/java/com/mednine/pillbuddy/domain/user/dto/JoinDto.java
@@ -8,9 +8,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class JoinDto {
 
     @NotBlank
---
+++ src/test/java/com/mednine/pillbuddy/domain/user/service/MyUserDetailsServiceTest.java
@@ -0,0 +1,47 @@
+package com.mednine.pillbuddy.domain.user.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.mednine.pillbuddy.domain.user.entity.CustomUserDetails;
+import com.mednine.pillbuddy.global.exception.PillBuddyCustomException;
+import java.util.Collection;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.GrantedAuthority;
+
+@SpringBootTest
+class MyUserDetailsServiceTest {
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Test
+    @DisplayName("회원의 login Id 를 통해 회원 정보를 조회할 수 있다.")
+    void loadUserByUsername() {
+        // given
+        String loginId = "caretaker1";
+
+        // when
+        CustomUserDetails details = myUserDetailsService.loadUserByUsername(loginId);
+        Collection<? extends GrantedAuthority> authorities = details.getAuthorities();
+
+        // then
+        assertThat(details.getUsername()).isEqualTo(loginId);
+        assertThat(details.getPassword()).isEqualTo("password1");
+        assertThat(authorities.size()).isEqualTo(1);
+        assertThat(authorities.iterator().next().getAuthority()).isEqualTo("USER");
+    }
+
+    @Test
+    @DisplayName("저장되지 않은 login Id 를 입력하면 PillBuddyCustomException 이 발생한다.")
+    void loadUserByUsername_exception() {
+        String loginId = "AnonymousUser1";
+
+        assertThatThrownBy(() -> myUserDetailsService.loadUserByUsername(loginId))
+                .isInstanceOf(PillBuddyCustomException.class)
+                .hasMessage("회원 정보를 찾을 수 없습니다.");
+    }
+}
