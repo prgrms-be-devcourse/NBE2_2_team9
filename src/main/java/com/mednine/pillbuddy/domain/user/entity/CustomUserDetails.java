b9cb4a698a65ca921e8bb55542d531812eb35235
hongjeZZ <anhongje98@naver.com>
2024-09-27 10:56:49 +0900

    feat: MyUserDetailsService 클래스 추가 및 UserDetails 구현

---
+++ src/main/java/com/mednine/pillbuddy/domain/user/entity/CustomUserDetails.java
@@ -0,0 +1,52 @@
+package com.mednine.pillbuddy.domain.user.entity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add((GrantedAuthority) () -> String.valueOf(user.getRole()));
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
