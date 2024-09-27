2a260ac27382b5f64df3734b92d3a6be434c8bdd
MisaSohee <sohee5951@naver.com>
2024-09-27 11:42:09 +0900

    feat: Notification 컨트롤러 구현 - createNotifications 메서드: userMedication 정보를 토대로 여러 notification 생성
    - checkAndSendNotifications 메서드:
    스케줄링를 이용하여 1분에 한 번씩 실행됨

--- src/main/java/com/mednine/pillbuddy/domain/notification/controller/NotificationController.java
+++ src/main/java/com/mednine/pillbuddy/domain/notification/controller/NotificationController.java
@@ -1,12 +1,16 @@
 package com.mednine.pillbuddy.domain.notification.controller;
 
+import com.mednine.pillbuddy.domain.notification.entity.Notification;
 import com.mednine.pillbuddy.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
-import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,16 +19,14 @@ public class NotificationController {
 
     private final NotificationService notificationService;
 
+    @PostMapping("/{userMedicationId}")
+    public ResponseEntity<List<Notification>> createNotifications(@PathVariable Long userMedicationId) {
+        List<Notification> notifications = notificationService.createNotificationsForUserMedication(userMedicationId);
+        return ResponseEntity.ok(notifications);
+    }
 
-    /**
-     * 단일 메시지 발송 예제
-     */
-    @PostMapping("/send-sms/{to}")
-    public SingleMessageSentResponse sendOne(@PathVariable("to") String to) {
-
-        SingleMessageSentResponse response = notificationService.sendSms(to);
-        System.out.println(response);
-
-        return response;
+    @Scheduled(fixedRate = 60000)
+    public void checkAndSendNotifications() {
+        notificationService.sendNotifications();
     }
 }
