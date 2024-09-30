3e97d6978fb874cc959a6d34cced4ebff07a06cf
jinwoo <dlwlsdn2628@hanmail.net>
2024-09-30 10:32:03 +0900

    사용자 등록, 삭제 기능

--- src/main/java/com/mednine/pillbuddy/domain/user/caregiver/repository/CaregiverRepository.java
+++ src/main/java/com/mednine/pillbuddy/domain/user/caregiver/repository/CaregiverRepository.java
@@ -2,8 +2,6 @@ package com.mednine.pillbuddy.domain.user.caregiver.repository;
 
 import com.mednine.pillbuddy.domain.user.caregiver.entity.Caregiver;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface CaregiverRepository extends JpaRepository<Caregiver, Long> {
-    Optional<Caregiver> findById(Long id);
 }
