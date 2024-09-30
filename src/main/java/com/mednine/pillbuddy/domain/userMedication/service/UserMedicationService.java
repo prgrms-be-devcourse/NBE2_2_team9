057761a6d188731ddfa04becf6e99983c153cc14
xxxkyeom <seonggyeomgim97@gmail.com>
2024-09-30 16:37:23 +0900

    24: [FEAT] 사용자 기능 API (약 복용 기록 조회) 구현 (#32)
    Co-authored-by: seongkyeom <xxxkyeom@github.com>

--- src/main/java/com/mednine/pillbuddy/domain/userMedication/service/UserMedicationService.java
+++ src/main/java/com/mednine/pillbuddy/domain/userMedication/service/UserMedicationService.java
@@ -1,7 +1,9 @@
 package com.mednine.pillbuddy.domain.userMedication.service;
 
+import com.mednine.pillbuddy.domain.record.dto.RecordDTO;
 import com.mednine.pillbuddy.domain.userMedication.dto.UserMedicationDTO;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface UserMedicationService {
@@ -11,4 +13,6 @@ public interface UserMedicationService {
     List<UserMedicationDTO> retrieve(Long caretakerId);
 
     UserMedicationDTO modify(Long caretakerId, Long userMedicationId, UserMedicationDTO userMedicationDTO);
+
+    List<RecordDTO> getUserMedicationRecordsByDate(Long caretakerId, LocalDateTime date);
 }
