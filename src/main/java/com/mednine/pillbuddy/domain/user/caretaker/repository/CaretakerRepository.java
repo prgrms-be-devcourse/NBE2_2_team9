569a92df43a03a994bac6f7360dd2474d0c9e8e8
jinwoo <dlwlsdn2628@hanmail.net>
2024-09-30 10:34:11 +0900

    .

--- src/main/java/com/mednine/pillbuddy/domain/user/caretaker/repository/CaretakerRepository.java
+++ src/main/java/com/mednine/pillbuddy/domain/user/caretaker/repository/CaretakerRepository.java
@@ -3,8 +3,5 @@ package com.mednine.pillbuddy.domain.user.caretaker.repository;
 import com.mednine.pillbuddy.domain.user.caretaker.entity.Caretaker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CaretakerRepository extends JpaRepository<Caretaker, Long> {
-    Optional<Caretaker> findById(Long id);
 }
