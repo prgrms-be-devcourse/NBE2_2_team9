e2a4d05daced31d7f627d1770bec8cc82242e8bb
MisaSohee <sohee5951@naver.com>
2024-09-30 15:03:07 +0900

    feat: 메세지 전송 api 사용 메서드 변경

--- src/main/java/com/mednine/pillbuddy/domain/notification/provider/SmsProvider.java
+++ src/main/java/com/mednine/pillbuddy/domain/notification/provider/SmsProvider.java
@@ -1,8 +1,9 @@
 package com.mednine.pillbuddy.domain.notification.provider;
 
 import net.nurigo.sdk.NurigoApp;
-import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
 import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -19,7 +20,6 @@ public class SmsProvider {
                        @Value("${sms.domain}") String DOMAIN) {
         this.messageService = NurigoApp.INSTANCE.initialize(API_KEY, API_SECRET_KEY, DOMAIN);
     }
-
     @Value("${sms.from-number}") String FROM;
 
     public void sendNotification(String phoneNumber, String medicationName) {
@@ -28,13 +28,7 @@ public class SmsProvider {
         message.setTo(phoneNumber);
         message.setText("약 복용 시간입니다: " + medicationName);
 
-        try {
-            messageService.send(message);
-        } catch (NurigoMessageNotReceivedException exception) {
-            System.out.println(exception.getFailedMessageList());
-            System.out.println(exception.getMessage());
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-        }
+        SingleMessageSentResponse response = messageService.sendOne(new SingleMessageSendingRequest(message));
+        System.out.println("메세지 전송 성공: " + response);
     }
 }
