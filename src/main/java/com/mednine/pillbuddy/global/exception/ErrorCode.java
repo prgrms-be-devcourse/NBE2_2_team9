6de96c3938a3e18db88d8443a95046c6b5ed9e87
Dia2Fan <woo93xna@naver.com>
2024-10-01 16:22:10 +0900

    fix: 약 정보 검색
    병합과정에서 삭제된 코드 추가

--- src/main/java/com/mednine/pillbuddy/global/exception/ErrorCode.java
+++ src/main/java/com/mednine/pillbuddy/global/exception/ErrorCode.java
@@ -10,6 +10,7 @@ import static org.springframework.http.HttpStatus.*;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
+    USER_AUTHENTICATION_REQUIRED(UNAUTHORIZED, "인증이 필요한 회원입니다."),
     USER_NOT_FOUND(NOT_FOUND, "회원 정보를 찾을 수 없습니다."),
     USER_ALREADY_REGISTERED_EMAIL(CONFLICT, "이미 등록된 이메일입니다."),
     USER_ALREADY_REGISTERED_LOGIN_ID(CONFLICT, "이미 등록된 아이디입니다."),
@@ -26,6 +27,7 @@ public enum ErrorCode {
     MEDICATION_NOT_REGISTERED(CONFLICT, "약 정보 등록에 실패했습니다."),
     MEDICATION_NOT_MODIFIED(CONFLICT, "약 정보 수정에 실패했습니다."),
     MEDICATION_NOT_VALID(BAD_REQUEST, "유효하지 않는 약 정보입니다."),
+    MEDICATION_IS_NULL(NOT_FOUND, "현재 복용중인 약이 없습니다."),
 
     CARETAKER_CAREGIVER_NOT_FOUND(CONFLICT, "부모 정보 등록에 실패했습니다"),
     CARETAKER_CAREGIVER_REMOVED(CONFLICT, "부모 정보 삭제에 실패했습니다"),
