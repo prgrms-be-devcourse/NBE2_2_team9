3add3906694f6db6729ad019a84624dce698d0c8
Dia2Fan <dd@dd>
2024-10-01 20:31:34 +0900

    fix: 약 정보 검색
    불필요한 import 삭제

--- src/main/java/com/mednine/pillbuddy/domain/medicationApi/controller/MedicationApiController.java
+++ src/main/java/com/mednine/pillbuddy/domain/medicationApi/controller/MedicationApiController.java
@@ -6,18 +6,14 @@ import com.mednine.pillbuddy.domain.medicationApi.dto.MedicationForm;
 import com.mednine.pillbuddy.domain.medicationApi.service.MedicationApiService;
 import com.mednine.pillbuddy.global.exception.ErrorCode;
 import com.mednine.pillbuddy.global.exception.PillBuddyCustomException;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController @RequiredArgsConstructor
 @RequestMapping("/api/search") @Slf4j
