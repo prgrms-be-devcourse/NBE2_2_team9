ee15a6d1723f90b1c2b2b1c151952efac5b304e8
hongjeZZ <anhongje98@naver.com>
2024-10-02 15:20:50 +0900

    test: ProfileService 테스트 클래스 수정

--- src/test/java/com/mednine/pillbuddy/domain/user/profile/service/ProfileServiceTest.java
+++ src/test/java/com/mednine/pillbuddy/domain/user/profile/service/ProfileServiceTest.java
@@ -1,50 +1,27 @@
 package com.mednine.pillbuddy.domain.user.profile.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.mockito.Mockito.when;
 
 import com.mednine.pillbuddy.domain.user.dto.UserType;
 import com.mednine.pillbuddy.domain.user.profile.dto.ProfileUploadDto;
-import com.mednine.pillbuddy.domain.user.profile.service.uploader.CaregiverProfileUploader;
-import com.mednine.pillbuddy.domain.user.profile.service.uploader.CaretakerProfileUploader;
-import com.mednine.pillbuddy.domain.user.profile.service.uploader.ProfileUploader;
 import com.mednine.pillbuddy.global.exception.ErrorCode;
 import com.mednine.pillbuddy.global.exception.PillBuddyCustomException;
-import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 
+@SpringBootTest
 class ProfileServiceTest {
 
-    @InjectMocks
+    @Autowired
     private ProfileService profileService;
-
-    @Mock
-    private CaregiverProfileUploader caregiverProfileUploader;
-
-    @Mock
-    private CaretakerProfileUploader caretakerProfileUploader;
-
-    @Mock
-    private Map<UserType, ProfileUploader> uploaderMap;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        when(uploaderMap.get(UserType.CAREGIVER)).thenReturn(caregiverProfileUploader);
-        when(uploaderMap.get(UserType.CARETAKER)).thenReturn(caretakerProfileUploader);
-    }
 
     @Test
     @DisplayName("잘못된 사용자 유형일 경우 예외가 발생한다.")
     void uploadProfile_invalid_userType() {
-        MockMultipartFile file = new MockMultipartFile("file", "test.jpg", "image/jpeg",
-                "test image content".getBytes());
+        MockMultipartFile file = new MockMultipartFile("file", "test.jpg", "image/jpeg", "test image content".getBytes());
         UserType userType = null;
 
         Long userId = 1L;
@@ -72,8 +49,7 @@ class ProfileServiceTest {
     @Test
     @DisplayName("파일의 ContentType 이 image 로 시작하지 않으면 예외가 발생한다.")
     void uploadProfile_with_invalid_content_type() {
-        MockMultipartFile file = new MockMultipartFile("file", "test.jpg", "audio/mpeg",
-                "test image content".getBytes());
+        MockMultipartFile file = new MockMultipartFile("file", "test.jpg", "audio/mpeg", "test image content".getBytes());
         UserType userType = UserType.CAREGIVER;
 
         Long userId = 1L;
