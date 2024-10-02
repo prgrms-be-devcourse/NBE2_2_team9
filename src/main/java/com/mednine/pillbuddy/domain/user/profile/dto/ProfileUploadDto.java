98785977b828bafb2257d25eb0a54d879218eb71
hongjeZZ <anhongje98@naver.com>
2024-10-02 12:27:24 +0900

    feat: ProfileUploadDto 제약 조건 추가

--- src/main/java/com/mednine/pillbuddy/domain/user/profile/dto/ProfileUploadDto.java
+++ src/main/java/com/mednine/pillbuddy/domain/user/profile/dto/ProfileUploadDto.java
@@ -1,6 +1,7 @@
 package com.mednine.pillbuddy.domain.user.profile.dto;
 
 import com.mednine.pillbuddy.domain.user.dto.UserType;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,6 +10,9 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 public class ProfileUploadDto {
 
+    @NotNull
     private MultipartFile file;
+
+    @NotNull
     private UserType userType;
 }
