337249e4c1d433c2d063736a9f2af3811191d804
hongjeZZ <anhongje98@naver.com>
2024-10-03 16:51:07 +0900

    test: 사용자 조회 테스트 코드 작성

--- src/main/java/com/mednine/pillbuddy/domain/user/dto/UserType.java
+++ src/main/java/com/mednine/pillbuddy/domain/user/dto/UserType.java
@@ -1,5 +1,12 @@
 package com.mednine.pillbuddy.domain.user.dto;
 
+import com.mednine.pillbuddy.domain.user.entity.User;
+
 public enum UserType {
-    CAREGIVER, CARETAKER
+    CAREGIVER, CARETAKER;
+
+    public static UserType from(User user) {
+        String userType = user.getClass().getSimpleName().toUpperCase();
+        return UserType.valueOf(userType);
+    }
 }
--- src/test/java/com/mednine/pillbuddy/domain/user/service/UserServiceTest.java
+++ src/test/java/com/mednine/pillbuddy/domain/user/service/UserServiceTest.java
@@ -3,13 +3,20 @@ package com.mednine.pillbuddy.domain.user.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.mednine.pillbuddy.domain.user.caregiver.entity.Caregiver;
+import com.mednine.pillbuddy.domain.user.caregiver.repository.CaregiverRepository;
+import com.mednine.pillbuddy.domain.user.caretaker.entity.Caretaker;
+import com.mednine.pillbuddy.domain.user.caretaker.repository.CaretakerRepository;
 import com.mednine.pillbuddy.domain.user.dto.JoinDto;
 import com.mednine.pillbuddy.domain.user.dto.LoginDto;
 import com.mednine.pillbuddy.domain.user.dto.UserDto;
 import com.mednine.pillbuddy.domain.user.dto.UserType;
+import com.mednine.pillbuddy.domain.user.entity.Role;
+import com.mednine.pillbuddy.global.exception.ErrorCode;
 import com.mednine.pillbuddy.global.exception.PillBuddyCustomException;
 import com.mednine.pillbuddy.global.jwt.JwtToken;
 import com.mednine.pillbuddy.global.jwt.JwtTokenProvider;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +35,24 @@ class UserServiceTest {
 
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private CaretakerRepository caretakerRepository;
+
+    @Autowired
+    private CaregiverRepository caregiverRepository;
+
+    private Caretaker caretaker;
+
+    private Caregiver caregiver;
+
+    @BeforeEach
+    void setup() {
+        createCaretaker();
+        createCaregiver();
+        caretakerRepository.save(caretaker);
+        caregiverRepository.save(caregiver);
+    }
 
     @Test
     @DisplayName("JoinDto 를 통해 회원가입을 할 수 있다.")
@@ -174,5 +199,83 @@ class UserServiceTest {
         assertThatThrownBy(() -> userService.reissueToken(refreshToken))
                 .isInstanceOf(PillBuddyCustomException.class)
                 .hasMessage("유효하지 않은 JWT 토큰입니다.");
+    }
+
+    @Test
+    @DisplayName("사용자를 조회할 수 있다.")
+    void findCaretaker() {
+        // given
+        Long userId = caretaker.getId();
+        UserType userType = UserType.from(caretaker);
+
+        // when
+        UserDto userDto = userService.findUser(userId, userType);
+
+        // then
+        assertThat(userDto.getUsername()).isEqualTo(caretaker.getUsername());
+        assertThat(userDto.getPhoneNumber()).isEqualTo(caretaker.getPhoneNumber());
+        assertThat(userDto.getEmail()).isEqualTo(caretaker.getEmail());
+        assertThat(userDto.getUserType()).isEqualTo(String.valueOf(userType).toLowerCase());
+    }
+
+    @Test
+    @DisplayName("보호자를 조회할 수 있다.")
+    void findCaregiver() {
+        // given
+        Long userId = caregiver.getId();
+        UserType userType = UserType.from(caregiver);
+
+        // when
+        UserDto userDto = userService.findUser(userId, userType);
+
+        // then
+        assertThat(userDto.getUsername()).isEqualTo(caregiver.getUsername());
+        assertThat(userDto.getPhoneNumber()).isEqualTo(caregiver.getPhoneNumber());
+        assertThat(userDto.getEmail()).isEqualTo(caregiver.getEmail());
+        assertThat(userDto.getUserType()).isEqualTo(String.valueOf(userType).toLowerCase());
+    }
+
+    @Test
+    @DisplayName("등록되지 않은 사용자를 조회하면 PillBuddyCustomException 이 발생한다.")
+    void findCaretaker_with_invalid_caretaker() {
+        Long userId = 99999L;
+        UserType userType = UserType.CARETAKER;
+
+        assertThatThrownBy(() -> userService.findUser(userId, userType))
+                .isExactlyInstanceOf(PillBuddyCustomException.class)
+                .hasMessage(ErrorCode.USER_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    @DisplayName("등록되지 않은 보호자를 조회하면 PillBuddyCustomException 이 발생한다.")
+    void findCaregiver_with_invalid_caregiver() {
+        Long userId = 99999L;
+        UserType userType = UserType.CAREGIVER;
+
+        assertThatThrownBy(() -> userService.findUser(userId, userType))
+                .isExactlyInstanceOf(PillBuddyCustomException.class)
+                .hasMessage(ErrorCode.USER_NOT_FOUND.getMessage());
+    }
+
+    private void createCaretaker() {
+        caretaker = Caretaker.builder()
+                .username("test-caretaker")
+                .loginId("test-loginId")
+                .password("test-password")
+                .email("test-email")
+                .phoneNumber("test-phoneNumber")
+                .role(Role.USER)
+                .build();
+    }
+
+    private void createCaregiver() {
+        caregiver = Caregiver.builder()
+                .username("test-caregiver")
+                .loginId("test-loginId")
+                .password("test-password")
+                .email("test-email")
+                .phoneNumber("test-phoneNumber")
+                .role(Role.USER)
+                .build();
     }
 }
