08515b6f91403457d01cdfcdf5207f02e3c531bf
MisaSohee <sohee5951@naver.com>
2024-10-07 10:08:26 +0900

    feat: RecordRepository 충돌 해결

--- src/main/java/com/mednine/pillbuddy/domain/record/repository/RecordRepository.java
+++ src/main/java/com/mednine/pillbuddy/domain/record/repository/RecordRepository.java
@@ -1,7 +1,13 @@
 package com.mednine.pillbuddy.domain.record.repository;
 
 import com.mednine.pillbuddy.domain.record.entity.Record;
+import com.mednine.pillbuddy.domain.userMedication.entity.UserMedication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
+    @Query("SELECT r FROM Record r WHERE r.userMedication = :userMedication")
+    List<Record> findByUserMedication(UserMedication userMedication);
 }
