b7c7d17b4769dd777e3b4d12ca498b42dafa6109
HongJe <anhongje98@naver.com>
2024-10-08 09:26:46 +0900

    69:[FEAT] 리프레시 토큰 관리 기능 (#70)

--- src/main/java/com/mednine/pillbuddy/domain/user/controller/UserController.java
+++ src/main/java/com/mednine/pillbuddy/domain/user/controller/UserController.java
@@ -1,24 +1,18 @@
 package com.mednine.pillbuddy.domain.user.controller;
 
-import com.mednine.pillbuddy.domain.user.dto.JoinDto;
-import com.mednine.pillbuddy.domain.user.dto.LoginDto;
 import com.mednine.pillbuddy.domain.user.dto.UserDto;
 import com.mednine.pillbuddy.domain.user.dto.UserPasswordUpdateDto;
 import com.mednine.pillbuddy.domain.user.dto.UserType;
 import com.mednine.pillbuddy.domain.user.dto.UserUpdateDto;
 import com.mednine.pillbuddy.domain.user.service.UserService;
-import com.mednine.pillbuddy.global.jwt.JwtToken;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,34 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-
-    @PostMapping("/join")
-    public ResponseEntity<UserDto> join(@Validated @RequestBody JoinDto joinDto) {
-        UserDto userDto = userService.join(joinDto);
-
-        return ResponseEntity.ok(userDto);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<JwtToken> login(@RequestBody LoginDto loginDto) {
-        JwtToken jwtToken = userService.login(loginDto);
-
-        return ResponseEntity.ok(jwtToken);
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<Void> logout() {
-        userService.logout();
-
-        return ResponseEntity.noContent().build();
-    }
-
-    @PostMapping("/reissue-token")
-    public ResponseEntity<JwtToken> reissueToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String bearerToken) {
-        JwtToken jwtToken = userService.reissueToken(bearerToken);
-
-        return ResponseEntity.ok(jwtToken);
-    }
 
     @GetMapping("/{userId}")
     public ResponseEntity<UserDto> findUserInfo(@PathVariable Long userId, UserType userType) {
---
+++ src/main/java/com/mednine/pillbuddy/global/redis/RedisUtils.java
@@ -0,0 +1,24 @@
+package com.mednine.pillbuddy.global.redis;
+
+import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisUtils {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public RedisUtils(@Qualifier(value = "BlackList") RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void setBlackList(String token, String loginId, Long milliSeconds) {
+        redisTemplate.opsForValue().set(token, loginId, milliSeconds, TimeUnit.MILLISECONDS);
+    }
+
+    public boolean hasTokenBlackList(String token) {
+        return redisTemplate.hasKey(token);
+    }
+}
