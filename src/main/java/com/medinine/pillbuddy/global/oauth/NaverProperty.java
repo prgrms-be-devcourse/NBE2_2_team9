0cfdffbe77f807bab3f22806066c6073825738fc
hongjeZZ <ajyong316@gmail.com>
2024-11-05 11:01:21 +0900

    refactor: Social Property 필드 상수화

--- src/main/java/com/medinine/pillbuddy/global/oauth/NaverProperty.java
+++ src/main/java/com/medinine/pillbuddy/global/oauth/NaverProperty.java
@@ -10,11 +10,7 @@ public class NaverProperty {
     public static String NAVER_CLIENT_SECRET;
     public static String NAVER_CAREGIVER_REDIRECT_URI;
     public static String NAVER_CARETAKER_REDIRECT_URI;
-    public static String NAVER_AUTHORIZATION_URI;
-    public static String NAVER_OAUTH_QUERY_STRING = "?response_type=code&client_id=%s&redirect_uri=%s";
-    public static String NAVER_TOKEN_URI;
     public static String NAVER_AUTHORIZATION_GRANT_TYPE;
-    public static String NAVER_USER_INFO_URI;
 
     @Value("${oauth.naver.client-id}")
     public void setNaverClientId(String naverClientId) {
@@ -36,23 +32,8 @@ public class NaverProperty {
         NAVER_CARETAKER_REDIRECT_URI = naverCaretakerRedirectUri;
     }
 
-    @Value("${oauth.naver.authorization-uri}")
-    public void setNaverAuthorizationUri(String naverAuthorizationUri) {
-        NAVER_AUTHORIZATION_URI = naverAuthorizationUri;
-    }
-
-    @Value("${oauth.naver.token-uri}")
-    public void setNaverTokenUri(String naverTokenUri) {
-        NAVER_TOKEN_URI = naverTokenUri;
-    }
-
     @Value("${oauth.naver.authorization-grant-type}")
     public void setNaverAuthorizationGrantType(String naverAuthorizationGrantType) {
         NAVER_AUTHORIZATION_GRANT_TYPE = naverAuthorizationGrantType;
-    }
-
-    @Value("${oauth.naver.user-info-uri}")
-    public void setNaverUserInfoUri(String naverUserInfoUri) {
-        NAVER_USER_INFO_URI = naverUserInfoUri;
     }
 }
